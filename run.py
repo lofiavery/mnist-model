2d1da08351fa9adef74518dadbf575e583b4410d
lofiavery <lofiavery@proton.me>
2018-02-28 22:20:08 +0200

    initial commit

---
+++ run.py
@@ -0,0 +1,17 @@
+import tensorflow as tf
+# import tensorflow.examples.tutorials.mnist.input_data as input_data
+# import numpy as np
+# import matplotlib.pyplot as plt
+# import sys
+# import os
+# #import cv2
+# import numpy as np
+# # def load_data(img_dir):
+# #     return np.array([cv2.imread(os.path.join(img_dir, img)).flatten() for img in os.listdir(img_dir) if img.endswith(".jpg")])
+# # print("ronen ronen")
+#
+# # read the data and labels as ont-hot vectors
+# # one-hot means a sparse vector for every observation where only
+# # the class label is 1, and every other class is 0.
+# # more info here:
+# mnist = input_data.read_data_sets('MNIST_data/', one_hot=True)
